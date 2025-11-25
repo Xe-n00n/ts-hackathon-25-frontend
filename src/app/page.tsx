56985f00135e3916ba1f624cf1c86b9c90ec70a1
Rawan El-Shishtawy <36961921+rawanshisht@users.noreply.github.com>
2025-11-25 00:51:01 +0000

    add logo to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-
+import Image from 'next/image';
 export default function Home() {
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8 flex flex-col items-center justify-center h-screen">
+      <Image src="/Main Logo.svg" alt="Hikaya Logo" width={100} height={100} className="mb-1" />
+      <Image src="/App Name.svg" alt="Hikaya Title" width={100} height={100} />
     </div>
   );
 }
